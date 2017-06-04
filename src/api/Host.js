89a764213bdd1044f3d7dba80875a58ad5566e84
wlyer <Administrator@SD-201609111228>
2017-06-04 18:24:06 +0800

    ip修改

--- src/api/Host.js
+++ src/api/Host.js
@@ -1,5 +1,5 @@
-export const Host = "http://192.168.11.117:8003/";
+export const Host = "http://xxxx:8003/";
 
 //export const Host = "http://xxxx:8011/";
 
-//export const Host = "http://192.168.11.117:8090/";
+//export const Host = "http://xxxx:8090/";
