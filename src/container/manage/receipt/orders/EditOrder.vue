e548770917a0c4834dd1183e7f731fa190f3e026
dell <dell@dell-PC>
2017-06-11 11:05:35 +0800

    订单管理

---
+++ src/container/manage/receipt/orders/EditOrder.vue
@@ -0,0 +1,166 @@
+<template>
+	<section class="order-edit">
+		<el-card>
+		  <div slot="header" class="clearfix">
+		    <strong style="line-height: 20px;">修改订单</strong>
+		  </div>
+		  	<div>
+		  		<el-card class="editOrder-one">
+			  		<div style="margin-bottom: 30px;"><strong>订单信息</strong></div>
+			  		<div>
+			  			<div  class="editOrder-div">
+			  				<div>订单号：<span v-text="editOrderForm.orderId"></span></div>
+			  				<div>收件人：<span v-text="editOrderForm.receiver"></span></div>
+			  			</div>
+			  			<div  class="editOrder-div">
+			  				<div>收件电话：<span v-text="editOrderForm.phone"></span></div>
+			  				<div>下单时间：<span v-text="editOrderForm.orderDate"></span></div>
+			  			</div>
+			  		</div>
+				 </el-card>
+			</div>
+		<!--编辑界面-->
+			<el-form :model="editOrderForm" label-width="100px" :rules="editFormRules" ref="editOrderForm">
+				<el-card style="padding-left: 1em;">
+					<div class="editOrder-tab"><strong>送货信息</strong></div>
+					<el-form-item label="送件人：">
+						<el-select  placeholder="请选择送件人" v-model="editOrderForm.delivery">
+					      <el-option label="员工1" value="0"></el-option>
+					      <el-option label="员工2" value="0"></el-option>
+					    </el-select>
+					</el-form-item>
+					<el-form-item label="送件人电话：">
+						<span v-text="editOrderForm.courierPhone"></span>
+					</el-form-item>
+					<el-form-item label="配送时间：">
+						<el-date-picker type="date" placeholder="选择日期" v-model="editOrderForm.deliveryDate"></el-date-picker>
+					</el-form-item>
+				</el-card>
+				<el-card style="padding-left: 1em;">
+					<div class="editOrder-tab"><strong>收货信息</strong></div>
+					<el-form-item label="收货时间：">
+						<el-date-picker type="date" placeholder="选择日期" v-model="editOrderForm.receiverDate"></el-date-picker>
+					</el-form-item>
+					<el-form-item label="状态：">
+						<el-select  placeholder="请选择状态" v-model="editOrderForm.state">
+					      <el-option label="待分配" value="0"></el-option>
+					      <el-option label="待收件" value="1"></el-option>
+					    </el-select>
+					</el-form-item>
+				</el-card>
+			</el-form>
+			<div class="editOrder-botton">
+				<el-button type="primary">浏览订单图书信息</el-button>
+				<el-button type="primary"  @click.native="back()">返回</el-button>
+				<el-button type="primary"  @click.native="editSubmit" >提交</el-button>
+			</div>
+		</el-card>
+	</section>
+	
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			editFormRules: {
+				delivery: [
+					{ required: true, message: '请选择送件人', trigger: 'blur' }
+				],
+				deliveryDate: [
+					{ required: true, message: '请输入送货时间', trigger: 'blur' }
+				],
+				receiverDate: [
+					{ required: true, message: '请输入收货时间', trigger: 'blur' }
+				],
+				state: [
+					{ required: true, message: '请选择状态', trigger: 'blur' }
+				]
+			},
+			editOrderForm: {
+				orderId: 'AAAAAAAA',
+				receiver: 'BBBBB',
+				phone: 'CCCCCC',
+				orderDate: 'DDDDD',
+				delivery: '',
+				deliveryDate: '',
+				courierPhone: '',
+				receiverDate: '',
+				state: ''
+			}
+		}
+	},
+	methods: {
+		//编辑
+		editSubmit() {
+			this.$refs.editOrederForm.validate((valid) => {
+				if (valid) {
+					this.$confirm('确认提交吗？', '提示', {}).then(() => {
+						let para = Object.assign({}, this.editOrederForm);
+//						para.time = (!para.time || para.time == '') ? '' : util.formatDate.format(new Date(para.time), 'yyyy-MM-dd');
+						this.$message({
+							message: '提交成功',
+							type: 'success'
+						});
+						this.$refs['editOrederForm'].resetFields();
+						this.$router.push({ path: '/recept/order' });
+					});
+				}
+			});
+		},
+		//返回
+		back(){
+			this.$refs.editOrderForm.resetFields();
+			this.$router.go(-1);
+		},
+	},
+	mounted() {
+		const id=this.$route.query.id;
+	},
+	components: {
+	}
+}
+</script>
+
+<style scoped lang="scss">
+.editOrder-botton{
+	margin-top: 1em;
+	text-align: center;
+}
+.order-edit{
+	padding-top: 50px;
+	width:65%;
+    margin: auto;
+    .clearfix:before,
+  	.clearfix:after {
+        display: table;
+        content: "";
+  	}
+	.clearfix:after {
+	    clear: both
+	}
+	.editOrder-one{
+		font-size: 14px;
+    	color: #48576a;
+    	padding-left: 2em;
+    	.el-row{
+    		margin-top: 2em;
+    	}
+    	.editOrder-div div{
+    		float: left;
+    		width: 50%;
+    		margin-bottom: 2em;
+    	}
+	}
+	.editOrder-tab{
+		margin-bottom: 2em;
+    	margin-left: 1em;
+	}
+}
+.el-row {
+    margin-bottom: 10px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+</style>
--- src/container/manage/receipt/orders/Order.vue
+++ src/container/manage/receipt/orders/Order.vue
@@ -1,15 +1,175 @@
 <template>
-	<section>
-		订单管理
-	</section>
+	<div class="order-container">
+		<div class="order-top">
+			<el-button type="primary" :disabled="this.sels.length===0" @click="batchRemove">删除</el-button>
+			<el-input
+			  placeholder="请输入关键字"
+			  icon="search"
+			  v-model="searchVaule"
+			  :on-icon-click="handleSearch">
+			</el-input>
+		</div>
+		<CommTable  :tableConfig="tableConfig" :mycolumns="columns" :tableData="tableData" :myrowOptions="rowOptions"></CommTable>
+	</div>
 </template>
 
 <script>
-	export default {
+import CommTable from '../../../../components/CommTable'
+export default {
+	components: {
+		CommTable
+	},
+	data() {
+		const columns = [{
+		        label: '订单编号',
+		        prop: 'orderId',
+		        handelLink: this.handelLink
+	        },
+	        {
+	          	label: '订单地址',
+	          	prop: 'address',
+	          	width: 150
+	        },
+	        {
+	          	label: '收件人',
+	          	prop: 'receiver',
+	        },
+	        {
+	          	label: '收件电话',
+	          	prop: 'phone',
+	          	width: 125
+	        },{
+	          	label: '下单时间',
+	          	prop: 'orderDate',
+	          	width: 110
+	        },
+	        {
+	          	label: '送货人',
+	          	prop: 'delivery',
+	        },
+	        {
+	          	label: '送货时间',
+	          	prop: 'deliveryDate',
+	          	width: 110
+	        },
+	        {
+	          	label: '状态',
+	          	prop: 'state',
+	          	width: 80,
+	        },
+	        {
+	          	label: "操作",
+	          	width: 190,
+		  		operations: [
+			  		{
+			          label: '修改',
+			          butType: 'info',
+			          func: this.handleEdit
+		        	},{
+			          label: this.labelFun,
+			          butType: 'info',
+			          func: this.handleAction
+		        	},{
+			          label: '删除',
+			          butType: 'danger',
+			          func: this.handleDelete
+		        	}
+		        ]
+	        }];
+	    let souData =[{
+	    	orderId: '20170868',
+	    	address: '喜马拉雅山脚下十一号',
+	    	receiver: '爱因斯坦',
+	    	phone: '13845668685',
+	    	orderDate: '2016-01-05',
+	    	delivery: '刘翔',
+	    	deliveryDate: '2016-10-06', 
+	    	state: '待配送'
+	    }];
+        return {
+        	columns,
+			tableData: souData,
+			rowOptions: this.handleSelectionChange,
+      		tableConfig: {
+		      	columns,
+		      	actionType: 'increment',
+		      	paramData: null,
+		      	tableData: souData,
+		      	total:0,
+		      	rowOptions:this.handleSelectionChange
+    		},
+    		searchVaule:null,//检索值
+    		sels: [],//列表选中列
+        }
+	},
+	methods: {
+		//关键字检索
+		handleSearch(ev) {
+			console.log(this.searchVaule);
+		},
+		//行中修改
+		handleEdit(row) {
+			
+		},
+		//行中删除
+		handleDelete(index, row) {
+			this.$confirm('确认删除该记录吗?', '提示', {
+				type: 'error'
+			}).then(() => {
+				this.$message({
+						message: '删除成功',
+						type: 'success'
+					});
+			});
+		},
+		//批量删除
+		batchRemove() {
+			var orderIds = this.sels.map(item => item.orderId).toString();
+			this.$confirm('确认删除选中记录吗？', '提示', {
+				type: 'error'
+			}).then(() => {
+				alert(orderIds)
+			})
+		},
+		//列表勾选的行
+		handleSelectionChange(val) {
+		    this.sels = val;
+	    },
+	    //转跳订单详情
+	    handelLink() {
+	    	this.$router.push({ path: '/receipt/order/editOrder' });
+	    },
+	    handleAction(index, row) {
+	    	alert("分配");
+	    },
+	    //状态不同显示不同操作
+	    labelFun(index,row) {
+	    	let str = '完成';
+	    	if(row.state == 1){
+	    		str = '分配';
+	    	}else if(row.state == 3){
+	    		str = '废止';
+	    	}
+	    	return str;
+	    }
 	}
+}
 
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.order-container{
+	margin-top: 10px;
+	width: 100%;
+	.order-top{
+		margin-bottom: 5px;
+		float: left;
+		width:100%;
+		.el-input{
+			width: 20%;
+			display: inline-block;
+			float: right;
+		}
+	}
+}
 </style>
