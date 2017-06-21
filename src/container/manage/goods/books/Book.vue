0d26eb9f1ab708a5e9e5c554ade7bdb20b84e61e
LongMr <1373837445@qq.com>
2017-06-21 16:48:09 +0800

    修改图书

--- src/container/manage/goods/books/Book.vue
+++ src/container/manage/goods/books/Book.vue
@@ -1,18 +1,18 @@
 <template>
 	<el-row style="margin-top: 10px;">
-		 <el-col :span="4">
+		<el-col :span="4">
 		 	<el-tree
-			  :data="data2"
+			  :data="bookTypeTree"
 			  :props="defaultProps"
-			  node-key="id"
+			  node-key="typeId"
 			  default-expand-all
 			  :expand-on-click-node="false"
-			  highlight-current="true"
+			  :highlight-current= true
 			  @node-click="handleClick"
 		  	>
-		</el-tree>
-		 </el-col>
-		 <el-col :span="19" style="margin-left: 5px;">
+			</el-tree>
+		</el-col>
+		<el-col :span="19" style="margin-left: 5px;">
 		 	<div class="book-top">
 				<el-button type="primary" @click="addSubmit">新增</el-button>
 				<el-button type="primary" :disabled="this.sels.length===0" @click="batchRemove">删除</el-button>
@@ -20,11 +20,13 @@
 				  placeholder="请输入关键字"
 				  icon="search"
 				  v-model="searchVaule"
-				  :on-icon-click="handleSearch">
+				  :on-icon-click="handleSearch"
+				  @keyup.enter.native="handleSearch"
+				  >
 				</el-input>
 			</div>
 			<CommTable  :tableConfig="tableConfig"></CommTable>
-		 </el-col>
+		</el-col>
 	</el-row>
 </template>
 
@@ -85,16 +87,6 @@ import { mapGetters } from 'vuex'
 		        	}
 		        ]
 	        }];
-	    /*模拟数据  */
-	    let souData = [{
-          bookId: '20160502',
-          bookName: '葵花宝典',
-          price: '￥ 20',
-          discount: '5',
-          storeDate: '1842-10-09',
-          storeHose: '黑木崖',
-          storeNum: '100'
-        }]
 	    //查询参数
         let params = {
         	pageNum: 1,
@@ -104,101 +96,107 @@ import { mapGetters } from 'vuex'
       return {
       		tableConfig: {
 		      	columns,
-		      	dispatch: 'increment',
+		      	dispatch: 'getByPage',
 		      	params,
 		      	dataList: [],
 		      	rowOptions:this.handleSelectionChange,
     		},
     		searchVaule: '',//检索值
     		sels: [],//列表选中列
-    		/*模拟数据*/
-	        data2: [{
-	          id: 1,
-	          label: '一级 1',
-		      children: [{
-		        id: 4,
-		        label: '二级 1-1',
-		        children: [{
-		          id: 9,
-		          label: '三级 1-1-1'
-		        }, {
-		          id: 10,
-		          label: '三级 1-1-2'
-		        }]
-		      }]
-			    }, {
-			      id: 2,
-			      label: '一级 2',
-			      children: [{
-			        id: 5,
-			        label: '二级 2-1'
-			      }, {
-			        id: 6,
-			        label: '二级 2-2'
-			      }]
-			    }, {
-			      id: 3,
-			      label: '一级 3',
-			      children: [{
-			        id: 7,
-			        label: '二级 3-1'
-			      }, {
-			        id: 8,
-			        label: '二级 3-2'
-			    }]
-			}],
+    		typeId:'',//分类id
 		    defaultProps: {
 		      	children: 'children',
-		      	label: 'label'
+		      	label: 'typeName'
 		    }
   		}
 	},
 	computed: {
 	 	...mapGetters([
-			'getByPage'
+			'getByPage',
+			'bookTypeTree'
    		])
 	},
 	watch: {
-		getByPage(){
-		  	this.$set(this.tableConfig, "dataList", this.getByPage.list);
-		  	this.$set(this.tableConfig, "params", this.params);
-		},
+		getByPage() {
+            this.$set(this.tableConfig, "dataList", this.getByPage);
+		}
 	},
 	methods: {
+		//初始化或者刷新表格
+		getBooks() {
+			let para = {
+				pageNum: this.getByPage.pageNum ? this.getByPage.pageNum : 1,
+				pageSize: this.getByPage.pageSize ? this.getByPage.pageSize : 10,
+				keyWord: this.searchVaule
+			};
+			this.$store.dispatch('getByPage',para);
+		},
 		//点击分类
 		handleClick(data) {
-			alert(data);
+			this.$store.dispatch('getBooksByBookTypeId',{bookTypeId: data.typeId});
+			this.typeId = data.typeId;
 		},
 		//新增
-		addSubmit: function () {
-			this.$router.push({ path: '/goods/book/addBook' });
+		addSubmit() {
+			this.$router.push({ path: '/goods/book/addBook' ,query: { typeId: this.typeId }});
 		},
 		//关键字检索
 		handleSearch(ev) {
-			console.log(this.searchVaule);
+			let para = {
+				pageNum: this.getByPage.pageNum ? this.getByPage.pageNum : 1,
+				pageSize: this.getByPage.pageSize ? this.getByPage.pageSize : 10,
+				keyWord: this.searchVaule,
+				typeId: this.typeId
+			};
+			this.$store.dispatch('getByPage',para);
 		},
 		//行中修改
-		handleEdit(row) {
-			
+		handleEdit(index, row) {
+			this.$router.push({ path: '/goods/book/editBook' ,query: { bookId: row.bookId }});
 		},
 		//行中删除
 		handleDelete(index, row) {
 			this.$confirm('确认删除该记录吗?', '提示', {
-				type: 'warning'
+				type: 'error'
 			}).then(() => {
-				this.$message({
-						message: '删除成功',
-						type: 'success'
-					});
+				let para = { bookId: row.bookId };
+				this.$store.dispatch('removeBook',para).then((res) => {
+					if(res.status==200){
+						this.$message({
+							message: '删除成功！',
+							type: 'success'
+						});
+						this.getBooks();
+					}else{
+						this.$message({
+							message: '删除失败！',
+							type: 'error'
+						});
+					}
+			    });  
 			});
 		},
 		//批量删除
-		batchRemove: function () {
-			var codes = this.sels.map(item => item.reclaimCode).toString();
+		batchRemove() {
+			let bookIds = this.sels.map(item => item.bookId).toString();
+			let para = {bookId:bookIds}
 			this.$confirm('确认删除选中记录吗？', '提示', {
-				type: 'warning'
+				type: 'error'
 			}).then(() => {
-				alert(codes)
+				this.$store.dispatch('removeBook',para).then((res) => {
+					if(res.status==200){
+						this.$message({
+							message: '删除成功！',
+							type: 'success'
+						});
+						this.getBooks();
+					}else{
+						this.$message({
+							message: '删除失败！',
+							type: 'error'
+						});
+					}
+			    });
 			})
 		},
 		//列表勾选的行
@@ -221,12 +219,12 @@ import { mapGetters } from 'vuex'
 	},
 	mounted() {
 		let para = {
-			pageNum: 1,
-			pageSize:10,
-			keyWord: this.searchVaule
-		};
-		this.para = Object.assign({},this.para, para);
-		this.$store.dispatch('getByPage',para);
+				pageNum: 1,
+				pageSize:  10,
+				keyWord: ''
+			};
+		this.$store.dispatch('getBookTypeTree',para);
+		this.getBooks();
 	},
 };
 </script>
--- src/container/manage/goods/books/EditBook.vue
+++ src/container/manage/goods/books/EditBook.vue
@@ -1,121 +1,121 @@
 <template>
-	<el-card class="addBook-card">
+	<el-card class="editBook-card">
 		<div slot="header" class="clearfix">
-		    <strong style="line-height: 5px;">新增图书</strong>
+		    <strong style="line-height: 5px;">修改图书</strong>
 		</div>
-		<el-form :model="addBookForm" label-width="100px" :rules="formRules" ref="addBookForm">
+		<el-form :model="editBookForm" label-width="100px" :rules="formRules" ref="editBookForm">
 			<el-row>
 				 <el-col :span="12">
 				 	<el-form-item label="图书编号：" prop="bookId">
-						<el-input v-model="addBookForm.bookId" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.bookId" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="图书名称：" prop="bookName">
-						<el-input v-model="addBookForm.bookName" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.bookName" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 			<el-row>
 				 <el-col :span="12">
 				 	<el-form-item label="作者：" prop="author">
-						<el-input v-model="addBookForm.author" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.author" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="价格：" prop="price">
-						<el-input v-model="addBookForm.price" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.price" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 			<el-row>
 				 <el-col :span="12">
 				 	<el-form-item label="折扣：" prop="discount">
-						<el-input v-model="addBookForm.discount" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.discount" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="出版社：" prop="press">
-						<el-input v-model="addBookForm.press" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.press" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 			<el-row>
 				 <el-col :span="12">
 				 	<el-form-item label="出版时间：" prop="publishTime">
-						<el-date-picker type="date" placeholder="选择日期" v-model="addBookForm.publishTime"></el-date-picker>
+						<el-date-picker type="date" placeholder="选择日期" v-model="editBookForm.publishTime"></el-date-picker>
 					</el-form-item>
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="版次：" prop="edition">
-						<el-input v-model="addBookForm.edition" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.edition" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 			<el-row>
 				 <el-col :span="12">
 				 	<el-form-item label="页数：" prop="pageNum">
-						<el-input v-model="addBookForm.pageNum" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.pageNum" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="字数：" prop="wordNum">
-						<el-input v-model="addBookForm.wordNum" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.wordNum" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 			<el-row>
 				 <el-col :span="12">
 				 	<el-form-item label="印刷时间：" prop="printTime">
-						<el-date-picker type="date" placeholder="选择日期" v-model="addBookForm.printTime"></el-date-picker>
+						<el-date-picker type="date" placeholder="选择日期" v-model="editBookForm.printTime"></el-date-picker>
 					</el-form-item>
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="开本：" prop="bookSize">
-						<el-input v-model="addBookForm.bookSize" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.bookSize" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 			<el-row>
 				 <el-col :span="12">
 				 	<el-form-item label="纸质：" prop="paper">
-						<el-input v-model="addBookForm.paper" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.paper" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="大图路径：" prop="image_l">
-						<el-input v-model="addBookForm.image_l" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.image_l" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 			<el-row>
 				 <el-col :span="12">
 				 	<el-form-item label="小图路径：" prop="image_s">
-						<el-input v-model="addBookForm.image_s" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.image_s" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="排序：" prop="orderBy">
-						<el-input v-model="addBookForm.orderBy" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.orderBy" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 			<el-row>
 				 <el-col :span="12">
-				 	<el-form-item label="仓库位置：" prop="storeHose">
-						<el-input v-model="addBookForm.storeHose" auto-complete="off"></el-input>
+				 	<el-form-item label="仓库位置：" prop="storeHouse">
+						<el-input v-model="editBookForm.storeHouse" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="库存量：" prop="storeNum">
-						<el-input v-model="addBookForm.storeNum" auto-complete="off"></el-input>
+						<el-input v-model="editBookForm.storeNum" auto-complete="off"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 			<el-row>
 				 <el-col :span="12">
 					<el-form-item label="状态：">
-						<el-select  placeholder="请选择状态" v-model="addBookForm.state">
+						<el-select  placeholder="请选择状态" v-model="editBookForm.state">
 					      <el-option label="待分配" value="0"></el-option>
 					      <el-option label="待收件" value="1"></el-option>
 					    </el-select>
@@ -123,24 +123,25 @@
 				 </el-col>
 				 <el-col :span="12">
 				 	<el-form-item label="图书简介：" prop="bookDesc">
-						<el-input type="textarea" v-model="addBookForm.bookDesc"></el-input>
+						<el-input type="textarea" v-model="editBookForm.bookDesc"></el-input>
 					</el-form-item>
 				 </el-col>
 			</el-row>
 		</el-form>
 		<div style="text-align: center;">
 			<el-button type="primary" @click.native="reset">重置</el-button>
-			<el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+			<el-button type="primary" @click.native="editSubmit" :loading="addLoading">提交</el-button>
 		</div>
 	</el-card>
 </template>
 
 <script>
 import util from '../../../../util/util'
+import { mapGetters } from 'vuex'
 export default {
 	data() {
 		return {
-			addBookForm: {
+			editBookForm: {
 				bookId: '',
 				bookName: '',
 				author: '',
@@ -157,7 +158,7 @@ export default {
 				image_l: '',
 				image_s: '',
 				orderBy: '',
-				storeHose: '',
+				storeHouse: '',
 				storeNum: '',
 				state: '',
 				bookDesc: ''
@@ -176,29 +177,60 @@ export default {
 				state: [
 					{ required: true, message: '请选择状态', trigger: 'blur' }
 				],
-				storeHose: [
+				storeHouse: [
 					{ required: true, message: '请选输入仓库位置', trigger: 'blur' }
 				]
 			},
 		}
 	},
+	computed: {
+		...mapGetters([
+			'getBookById'
+   		]),
+	},
+	watch: {
+		getBookById() {
+			let bookData ={
+					bookId: this.getBookById.bookId? this.getBookById.bookId: '',
+					bookName: this.getBookById.bookName? this.getBookById.bookName: '',
+					author: this.getBookById.author? this.getBookById.author: '',
+					price: this.getBookById.price? this.getBookById.price: '',
+					discount: this.getBookById.discount? this.getBookById.discount: '',
+					press: this.getBookById.press? this.getBookById.press: '',
+					publishTime: this.getBookById.publishTime? this.getBookById.publishTime: '',
+					edition: this.getBookById.edition? this.getBookById.edition: '',
+					pageNum: this.getBookById.pageNum? this.getBookById.pageNum: '',
+					wordNum: this.getBookById.wordNum? this.getBookById.wordNum: '',
+					printTime: this.getBookById.printTime? this.getBookById.printTime: '',
+					bookSize: this.getBookById.bookSize? this.getBookById.bookSize: '',
+					paper: this.getBookById.paper? this.getBookById.paper: '',
+					image_l: this.getBookById.image_l? this.getBookById.image_l: '',
+					image_s: this.getBookById.image_s? this.getBookById.image_s: '',
+					orderBy: this.getBookById.orderBy? this.getBookById.orderBy: '',
+					storeHouse: this.getBookById.storeHouse? this.getBookById.storeHouse: '',
+					storeNum: this.getBookById.storeNum? this.getBookById.storeNum: '',
+					state: this.getBookById.state? this.getBookById.state: '',
+					bookDesc: this.getBookById.bookDesc? this.getBookById.bookDesc: ''
+   				}
+			this.editBookForm = Object.assign({},this.editBookForm, bookData);
+		}
+	},
 	methods: {
-		addSubmit() {
-			this.$refs.addBookForm.validate((valid) => {
+		editSubmit() {
+			this.$refs.editBookForm.validate((valid) => {
 				if (valid) {
 					this.addLoading = true;
-					let typeId=this.$route.query.typeId;
-					let para = Object.assign({}, {typeId: typeId}, this.addBookForm);
+					let para = Object.assign({}, this.getBookById, this.editBookForm);
 					para.publishTime = (!para.publishTime || para.publishTime == '') ? '' : util.formatDate.format(new Date(para.publishTime), 'yyyy-MM-dd');
 					para.printTime = (!para.printTime || para.printTime == '') ? '' : util.formatDate.format(new Date(para.printTime), 'yyyy-MM-dd');
-					this.$store.dispatch('addBook',para).then((res) => {
+					this.$store.dispatch('editBook',para).then((res) => {
 						this.addLoading = false;
 						if(res.status==200){
 							this.$message({
 								message: res.msg,
 								type: 'success'
 							});
-							this.$refs['addBookForm'].resetFields();
+							this.$refs['editBookForm'].resetFields();
 							this.$router.push({ path: '/goods/book' });
 						}else{
 							this.$message({
@@ -212,14 +244,18 @@ export default {
 		},
 		//重置
 		reset() {
-			this.$refs.addBookForm.resetFields();
+			this.$refs.editBookForm.resetFields();
 		}
-	}
+	},
+	mounted() {
+		let bookId=this.$route.query.bookId;
+		this.$store.dispatch('getById',{bookId: bookId});
+	},
 }
 	
 </script>
 <style scoped lang="scss">
-.addBook-card {
+.editBook-card {
 	width:65%;
     margin: auto;
 	.clearfix:before,
