0b2f7ae0247858786abd3389fddd4f8342a64115
LongMr <1373837445@qq.com>
2017-06-27 21:51:16 +0800

    更新

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,6 @@ Vue.use(Vuex);
 import user from './User';
 import role from './Role';
 import log from './Log';
-import purview from './Purview';
 import books from './Books';
 import bookTypes from './BookTypes';
 import orders from './Orders';
@@ -16,8 +15,8 @@ export default new Vuex.Store({
     modules: {
         user,
         role,
-        log,
         purview,
+        log,
         books,
         bookTypes,
         orders,
